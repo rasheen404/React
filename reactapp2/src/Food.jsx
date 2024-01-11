8bb6f21e5906bf576eab3cf98a59b5b3807ab6f7
Rasheen <rasheenerror404@gmail.com>
2024-01-11 12:35:55 +0530

    learned  about header,footer,and component

---
+++ reactapp2/src/Food.jsx
@@ -0,0 +1,17 @@
+
+
+function Food()
+{
+    const Food1 = "Apple";
+    const Food2 = "Orange";
+
+    return(
+        <ul>
+            <li>Banana</li>
+            <li>{Food1}</li>
+            <li>{Food2}</li>
+        </ul>
+    )
+}
+
+export default Food
---
+++ reactapp2/src/Header.jsx
@@ -0,0 +1,17 @@
+function Header()
+{
+return (
+    <header>
+        <h1>My Website.</h1>
+        <nav>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Service</a></li>
+        </nav>
+        <hr></hr>
+
+    </header>
+)
+}
+
+export default Header
